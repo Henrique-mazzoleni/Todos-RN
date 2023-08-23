b3bf066fce51e968bda936cf62bb39e5af8ce048
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2023-08-23 22:27:46 +0200

    init commit

--- App.js
+++ App.js
@@ -1,11 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import {
+  Button,
+  FlatList,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
 
 export default function App() {
+  const [enteredTodo, setEnteredTodo] = useState('');
+  const [todosList, setTodosList] = useState([]);
+
+  const textInputHandler = (enteredText) => {
+    setEnteredTodo(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setTodosList((prevState) => [
+      ...prevState,
+      {
+        text: enteredTodo,
+        id: Math.random().toString(),
+      },
+    ]);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Enter your Todo"
+          style={styles.textInput}
+          onChangeText={textInputHandler}
+        />
+        <Button title="Add Todo" onPress={addGoalHandler} />
+      </View>
+      <View style={styles.listContainer}>
+        <FlatList
+          data={todosList}
+          renderItem={(todoData) => {
+            return (
+              <View style={styles.todoItem}>
+                <Text style={styles.todoText}>{todoData.item.text}</Text>
+              </View>
+            );
+          }}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +56,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
     backgroundColor: '#fff',
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    gap: 10,
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+    marginBottom: 20,
+  },
+  textInput: {
+    width: '70%',
+    borderWidth: 2,
+    borderColor: '#ccc',
+    padding: 10,
+  },
+  listContainer: {
+    flex: 5,
+  },
+  todoItem: {
+    backgroundColor: '#5e0acc',
+    borderRadius: 6,
+    padding: 8,
+    marginBottom: 8,
+  },
+  todoText: {
+    color: '#fff',
   },
 });
